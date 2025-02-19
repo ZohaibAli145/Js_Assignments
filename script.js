b140a9df2cef3bdda3f145c2c23a8c6bbb29bed8
Zohaib Ali <156583311+ZohaibAli145@users.noreply.github.com>
2025-02-19 10:32:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1. Declare a variable called username.
+// first Assignment
+// let username;
+
+// let myName = "Zohaib Ali";
+
+// let message;
+
+// message = "Hello World";
+// message = "17 years old";
+
+//     alert(message);
+
+// *************************************************************
+
+
+//Second Assignment
+// var age = 24;
+
+// alert ("My age is : " + age);
+
+//**************************************************** 
+  
+
+// Third Assignment  
+
+// Legal Variable Names:
+
+   // Starts with a letter
+ // Starts with a dollar sign
+  // Starts with an underscore
+ // Can include numbers (not at the start)
+  // Variable names are case-sensitive
+
+  //***********************************************
+
+
+  //Fourth Assignment
+//   let num1 = 5;
+//   let num2 = 10;
+
+//   let sum = num1 + num2;
+
+//   document.write(sum);
+
+
+//******************************************************** */
+
+// Fifth Assignment
