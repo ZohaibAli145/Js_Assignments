d22d80bc1a33255396e6fcd269a3f79b8f58f975
Zohaib Ali <156583311+ZohaibAli145@users.noreply.github.com>
2025-02-19 10:34:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,172 @@
-// 1. Declare a variable called username.
-// first Assignment
-// let username;
-
-// let myName = "Zohaib Ali";
-
-// let message;
-
-// message = "Hello World";
-// message = "17 years old";
-
-//     alert(message);
-
-// *************************************************************
+//Question 7
 
 
-//Second Assignment
-// var age = 24;
+// var fruites = ['Apple' , 'mango', 'graphes'];
+ 
 
-// alert ("My age is : " + age);
+// fruites.sort()
+// document.write(fruites)
 
-//**************************************************** 
+
+// var Cities = ['Karachi', 'Lahore', 'Islamabad', 'Quetta', 'Peshawar']
+
+//  var SelectedCities= Cities.slice(2,4);   
+// document.write(SelectedCities);
+
+
+//Question 9
+
+// var FirstName = [1,2,3,4,5];
+
+// var LastName = FirstName.join('');
+
+// document.write(FirstName)
+
+
+// let arr = ['apple', 'banana', 'cherry'];
+// let result = arr.join(' ');
+// console.log(result); 
+
+
+// Question 10
+// let favoriteCars = ["Tesla Model S", "Ferrari LaFerrari", "Lamborghini Aventador", "Porsche 911", "Audi R8"];
+// let firstIndex = 0;
+// let carAtFirstIndex = favoriteCars[firstIndex];
+// let lastIndex = favoriteCars.length -1;
+// let carAtLastIndex = favoriteCars[lastIndex];
+
+// console.log("First index:", firstIndex);
+// console.log("Car at first index:", carAtFirstIndex);
+// console.log("Last index:", lastIndex);
+// console.log("Car at last index:", carAtLastIndex);
+
+// Question 11
+// let students = ['Michael', 'John', 'Tony'];
+// let scores = [320, 230, 480];
+
+// let michaelPercentage = (scores[0] / 500) * 100;
+// let johnPercentage = (scores[1] / 500) * 100;
+// let tonyPercentage = (scores[2] / 500) * 100;
+
+// console.log(`Score of ${students[0]} is ${scores[0]}. Percentage: ${michaelPercentage}%`);
+// console.log(`Score of ${students[1]} is ${scores[1]}. Percentage: ${johnPercentage}%`);
+// console.log(`Score of ${students[2]} is ${scores[2]}. Percentage: ${tonyPercentage}%`);
+
+
+// Question 13
+
+// let scores = [320, 230, 480, 120];
+
+//         console.log("Scores of Students:", scores.join(", "));
+
+//         scores.sort((a, b) => a - b); 
+//         console.log("Ascending order of scores:", scores.join(", "));
+
+
+
+// Question 14 
+// var fruits = ["strawberry", "apple", "orange", "banana"];
+
+// console.log("Original Array of Fruits:", fruits.join(", "));
+
+// fruits.sort();
+
+// console.log("Sorted Array of Fruits:", fruits.join(", "));
+
+// Question 15
+
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+//         var selectedCities = cities.slice(2, 4); 
+       
+//         console.log("Cities list:", cities.join(", "));
+//         console.log("Selected cities list:", selectedCities.join(", "));
+
+
+// Question 16
+// var arr = ["This ", " is ", " my ", " cat"];
+
+//         var result = arr.join("");
+//         console.log("Resulting string:", result);
+
+// Question 17
+
+// let devices = [];
+
+//         devices.push("keyboard");
+//         devices.push("mouse");
+//         devices.push("printer");
+//         devices.push("monitor");
+
+//         console.log("Devices in FIFO order:", devices);
+//         console.log("First device removed:", devices.shift());  
+//         console.log("Devices after first removal:", devices);
+
+//         console.log("Second device removed:", devices.shift()); 
+//         console.log("Devices after second removal:", devices);
+
+//         console.log("Third device removed:", devices.shift());  
+//         console.log("Devices after third removal:", devices);
+
+//         console.log("Last device removed:", devices.shift());   
+//         console.log("Devices after last removal:", devices);
+
+// Question 18 
+
+// let devices = [];
+
+//         devices.push("keyboard");
+//         devices.push("mouse");
+//         devices.push("printer");
+//         devices.push("monitor");
+
+        
+//         console.log("Devices in LIFO order:", devices);
+
+       
+//         console.log("First device removed:", devices.pop());  
+//         console.log("Devices after first removal:", devices);
+
+//         console.log("Second device removed:", devices.pop()); 
+//         console.log("Devices after second removal:", devices);
+
+//         console.log("Third device removed:", devices.pop());  
+//         console.log("Devices after third removal:", devices);
+
+//         console.log("Last device removed:", devices.pop());   
+//         console.log("Devices after last removal:", devices); 
+
+// Question 19 
+
+// var manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+//         document.write("<select>");
+//         document.write("<option value='" + manufacturers[0] + "'>" + manufacturers[0] + "</option>");
+//         document.write("<option value='" + manufacturers[1] + "'>" + manufacturers[1] + "</option>");
+//         document.write("<option value='" + manufacturers[2] + "'>" + manufacturers[2] + "</option>");
+//         document.write("<option value='" + manufacturers[3] + "'>" + manufacturers[3] + "</option>");
+//         document.write("<option value='" + manufacturers[4] + "'>" + manufacturers[4] + "</option>");
+//         document.write("<option value='" + manufacturers[5] + "'>" + manufacturers[5] + "</option>");
+//         document.write("</select>");
+
+
+// Question 20
+
+// let multiArray = [];
+
+// multiArray.push([1, 2, 3]);
+// multiArray.push([4, 5, 6]);
+// multiArray.push([7, 8, 9]);
+
+// console.log(multiArray);
+
+
+// Question 21
+
+// let matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+//   ];
   
-
-// Third Assignment  
-
-// Legal Variable Names:
-
-   // Starts with a letter
- // Starts with a dollar sign
-  // Starts with an underscore
- // Can include numbers (not at the start)
-  // Variable names are case-sensitive
-
-  //***********************************************
-
-
-  //Fourth Assignment
-//   let num1 = 5;
-//   let num2 = 10;
-
-//   let sum = num1 + num2;
-
-//   document.write(sum);
-
-
-//******************************************************** */
-
-// Fifth Assignment
+//   console.log(matrix);
